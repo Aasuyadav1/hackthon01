87526b5c71509fbe2ab72106abc2560adde441dc
aasuyadav1 <aasuy284@gmail.com>
2024-10-26 07:00:25 +0530

    fix: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,10 @@ import ProductUploader from "@/components/product-uploader";
 import UploadWidget from "@/components/cloudinary/upload-widget";
 import Hero from "@/components/landing-cmp/hero";
 import ProductDetails from "@/components/products/product-details";
-<<<<<<< HEAD
-import InfluencerCard from "@/components/influencers/influencer-card";
-=======
 import Influencer from "@/components/influencers/influencer";
 import InfluencerAdmin from "@/components/admin-dash/influencer-admin";
 import InstaInput from "@/components/insta-input";
 import InstaCard from "@/components/insta-card";
->>>>>>> 9bde63aba009bc100d88b5072bf0388425372ad3
 
 export default function Home() {
   return (
@@ -25,14 +21,10 @@ export default function Home() {
     <main className="h-dvh w-full">
       <Hero />
       <ProductDetails />
-<<<<<<< HEAD
-      <InfluencerCard/>
-=======
       <Influencer/>
       <InfluencerAdmin/>
       <InstaInput/>
       <InstaCard/>
->>>>>>> 9bde63aba009bc100d88b5072bf0388425372ad3
     </main>
   );
 }
--- src/components/landing-cmp/hero.tsx
+++ src/components/landing-cmp/hero.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from "react";
 import ProductUploader from "../product-uploader";
 import Image from "next/image";
